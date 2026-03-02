abf616241aa0c61b90ac990c2b66a82cd8f8e5bb
jackschu <31808950+jackschu@users.noreply.github.com>
2026-03-02 16:39:47 -0500

    try setup github actions to just build the derivations

--- bootstrap-stub/flake.nix
+++ bootstrap-stub/flake.nix
@@ -1,1 +1,16 @@
-{ outputs = _: { }; }
+{
+  outputs = _: {
+    nixosModules.host =
+      { lib, ... }:
+      {
+        options.services.agent-microvm = {
+          enable = lib.mkEnableOption "agent microvm";
+          externalInterface = lib.mkOption {
+            type = lib.types.str;
+            default = "";
+            description = "Host interface used by the agent microvm.";
+          };
+        };
+      };
+  };
+}
